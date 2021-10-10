7a170e3682aeda1d00d828be54087ae522195c06
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-10-10 02:55:06 +0200

    Add redis and configure it to be default queue driver
    Also add some route testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Jobs\LogToFile;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// redis
+//..
+Route::get('/test-redis-cache', function () {
+    Redis::set('name', 'Ahmed Helal');
+    dd(Redis::get('name'));
+});
+
+// database
+//..
+Route::get('/test-database', function () {
+    if(!\App\Models\User::count()){
+        \App\Models\User::factory()->create();
+    }
+    dd(\App\Models\User::first()->toArray());
+});
+
+// redis as queue driver
+//..
+Route::get('/test-redis-queue', function () {
+    dispatch(new LogToFile);
+    $queueName = 'default';
+    dd(
+        'Event dispatched',
+        Redis::keys('*'),
+        Redis::llen('queues:' . $queueName)
+    );
 });
